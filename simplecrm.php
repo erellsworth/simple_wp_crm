c81408880c712bb3bb15b59fa2e4ba924363c9e7
Edward <edward.ellsworth@gmail.com>
2018-03-12 17:04:12 -0700

    base files

---
+++ simplecrm.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package SimpleCRM
+ * @version 1
+ */
+/*
+Plugin Name: Simple CRM
+Description: Collect customer data and build customer profiles inside of the client’s WordPress Dashboard.
+Author: E.R. Ellsworth
+Plugin URI: https://erellsworth.com/wordpress
+Version: 1
+Author URI: https://erellsworth.com
+*/
+
+define('SIMPLECRM_PATH', dirname(__FILE__));
+
+require_once SIMPLECRM_PATH . '/post-type.php';
+require_once SIMPLECRM_PATH . '/admin.php';
