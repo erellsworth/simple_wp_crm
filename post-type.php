9a11aca3245ac77ff7ff50bf1775f64d7e59fe35
Edward <edward.ellsworth@gmail.com>
2018-03-13 19:52:25 -0700

    add custom taxonomies

--- post-type.php
+++ post-type.php
@@ -1,11 +1,11 @@
 <?php
 if(!class_exists('WP_Simple_CRM_PostType')){
 	class WP_Simple_CRM_PostType{
-		private $post_type, $labels;
+		private $post_type, $post_labels, $tag_labels, $cat_labels;
 		
 	    function __construct() {
 	    	$this->post_type = apply_filters('simple_crm_post_type', 'customer');
-	    	$this->labels = array(
+	    	$this->post_labels = array(
 				'name'                  => _x( 'Customers', 'Post Type General Name', 'simple_crm' ),
 				'singular_name'         => _x( 'Customer', 'Post Type Singular Name', 'simple_crm' ),
 				'menu_name'             => __( 'Customers', 'simple_crm' ),
@@ -34,6 +34,53 @@ if(!class_exists('WP_Simple_CRM_PostType')){
 				'items_list_navigation' => __( 'Customer list navigation', 'simple_crm' ),
 				'filter_items_list'     => __( 'Filter Customer list', 'simple_crm' ),
 			);
+
+			$this->tag_labels = array(
+				'name'                       => _x( 'Customer Tags', 'Taxonomy General Name', 'simple_crm' ),
+				'singular_name'              => _x( 'Customer Tag', 'Taxonomy Singular Name', 'simple_crm' ),
+				'menu_name'                  => __( 'Customer Tags', 'simple_crm' ),
+				'all_items'                  => __( 'All Customer Tags', 'simple_crm' ),
+				'parent_item'                => __( 'Parent Customer Tag', 'simple_crm' ),
+				'parent_item_colon'          => __( 'Parent Customer Tag:', 'simple_crm' ),
+				'new_item_name'              => __( 'New Customer Tag', 'simple_crm' ),
+				'add_new_item'               => __( 'Add New Customer Tag', 'simple_crm' ),
+				'edit_item'                  => __( 'Edit Customer Tag', 'simple_crm' ),
+				'update_item'                => __( 'Update Customer Tag', 'simple_crm' ),
+				'view_item'                  => __( 'View Customer Tag', 'simple_crm' ),
+				'separate_items_with_commas' => __( 'Separate customer tags with commas', 'simple_crm' ),
+				'add_or_remove_items'        => __( 'Add or remove customer tags', 'simple_crm' ),
+				'choose_from_most_used'      => __( 'Choose from the most used', 'simple_crm' ),
+				'popular_items'              => __( 'Popular Customer Tags', 'simple_crm' ),
+				'search_items'               => __( 'Search Customer Tags', 'simple_crm' ),
+				'not_found'                  => __( 'Not Found', 'simple_crm' ),
+				'no_terms'                   => __( 'No customer tags', 'simple_crm' ),
+				'items_list'                 => __( 'Customer Tag list', 'simple_crm' ),
+				'items_list_navigation'      => __( 'Customer Tag navigation', 'simple_crm' ),
+			);
+
+		$this->cat_labels = array(
+				'name'                       => _x( 'Customer Categories', 'Taxonomy General Name', 'simple_crm' ),
+				'singular_name'              => _x( 'Customer Category', 'Taxonomy Singular Name', 'simple_crm' ),
+				'menu_name'                  => __( 'Customer Categories', 'simple_crm' ),
+				'all_items'                  => __( 'All Customer Categories', 'simple_crm' ),
+				'parent_item'                => __( 'Parent Customer Category', 'simple_crm' ),
+				'parent_item_colon'          => __( 'Parent Customer Category:', 'simple_crm' ),
+				'new_item_name'              => __( 'New Customer Category', 'simple_crm' ),
+				'add_new_item'               => __( 'Add New Customer Category', 'simple_crm' ),
+				'edit_item'                  => __( 'Edit Customer Category', 'simple_crm' ),
+				'update_item'                => __( 'Update Customer Category', 'simple_crm' ),
+				'view_item'                  => __( 'View Customer Category', 'simple_crm' ),
+				'separate_items_with_commas' => __( 'Separate customer categories with commas', 'simple_crm' ),
+				'add_or_remove_items'        => __( 'Add or remove customer categories', 'simple_crm' ),
+				'choose_from_most_used'      => __( 'Choose from the most used', 'simple_crm' ),
+				'popular_items'              => __( 'Popular Customer Categories', 'simple_crm' ),
+				'search_items'               => __( 'Search Customer Categories', 'simple_crm' ),
+				'not_found'                  => __( 'Not Found', 'simple_crm' ),
+				'no_terms'                   => __( 'No customer categories', 'simple_crm' ),
+				'items_list'                 => __( 'Customer Category list', 'simple_crm' ),
+				'items_list_navigation'      => __( 'Customer Category navigation', 'simple_crm' ),
+			);
+
 	    }
 
 	    public function get_post_type(){
@@ -45,9 +92,9 @@ if(!class_exists('WP_Simple_CRM_PostType')){
 			$args = array(
 				'label'                 => __( 'Customer', 'simple_crm' ),
 				'description'           => __( 'Customer contacts', 'simple_crm' ),
-				'labels'                => $this->labels,
+				'labels'                => $this->post_labels,
 				'supports'              => array( 'title', 'editor' ),
-				'taxonomies'            => array( 'category', 'post_tag' ),
+				'taxonomies'            => array('customer_cats', 'customer_tags'),
 				'hierarchical'          => false,
 				'public'                => false,
 				'show_ui'               => true,
@@ -65,10 +112,39 @@ if(!class_exists('WP_Simple_CRM_PostType')){
 
 			register_post_type($this->post_type, $args );    	
 	    }
+
+	    public function register_tags(){
+
+			$args = array(
+				'labels'                     => $this->tag_labels,
+				'hierarchical'               => false,
+				'public'                     => false,
+				'show_ui'                    => true,
+				'show_admin_column'          => true,
+				'show_in_nav_menus'          => false,
+				'show_tagcloud'              => false,
+			);
+			register_taxonomy( 'customer_tags', array( $this->post_type ), $args );	    	
+	    }
+
+	    public function register_cats(){
+			$args = array(
+				'labels'                     => $this->cat_labels,
+				'hierarchical'               => true,
+				'public'                     => false,
+				'show_ui'                    => true,
+				'show_admin_column'          => true,
+				'show_in_nav_menus'          => false,
+				'show_tagcloud'              => false,
+			);
+			register_taxonomy( 'customer_cats', array( $this->post_type ), $args );	    	
+	    }
 	}
 	
 	$WP_Simple_CRM_PostType = new WP_Simple_CRM_PostType();
 
 
 	add_action('init', array($WP_Simple_CRM_PostType, 'register'));	
+	add_action( 'init', array($WP_Simple_CRM_PostType, 'register_tags'), 0 );
+	add_action( 'init', array($WP_Simple_CRM_PostType, 'register_cats'), 0 );	
 }
